3962bc2de54560be10323dc9cc242d93f1521404
Joelson Junior <joelson.ejr@gmail.com>
2021-05-06 12:49:42 -0300

    one more day

---
+++ 01-github-explorer/src/App.jsx
@@ -0,0 +1,3 @@
+export function App() {
+  return <h1>HelloWorld</h1>
+}
--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -1,12 +1,31 @@
-const path = require('path') //importando 'path' do node.
+const path = require("path"); //importando 'path' do node.
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.js'), 
+  mode: "development",
+  entry: path.resolve(__dirname, "src", "index.jsx"),
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   resolve: {
-    extensions: ['.js', '.jsx']
-  }
-}
+    extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html")
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+};
